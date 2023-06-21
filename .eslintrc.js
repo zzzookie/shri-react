c0d9a3642e23bf59e5ba73a1304a4c791bef45a2
Ivan Shulgin <multizookie@gmail.com>
2023-06-21 20:48:43 +0700

    Инициализация

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    "no-console": "off",
+    "react/jsx-filename-extension": 0,
+    "jsx-a11y/click-events-have-key-events": 0,
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": 0,
+    "no-unused-vars": 0,
+    "react/no-array-index-key": 0,
+    "jsx-a11y/anchor-is-valid": 0,
+    "jsx-a11y/label-has-associated-control": 0,
+    "jsx-a11y/no-static-element-interactions": 0,
+    "react/jsx-no-constructed-context-values": 0,
+    "default-param-last": 0,
+    quotes: 0
+  }
+}
