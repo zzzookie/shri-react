44c55006417f701146929ddebe7b09c88178f9d4
Ivan Shulgin <multizookie@gmail.com>
2023-06-27 03:36:25 +0700

    Добавил предзагрузку кириллических шрфитов

--- app/services/fonts.ts
+++ app/services/fonts.ts
@@ -1,4 +1,4 @@
 import { Roboto_Mono, Roboto } from "next/font/google";
 
-export const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
-export const roboto_mono = Roboto_Mono({ subsets: ['latin'], display: 'swap' })
+export const roboto = Roboto({ subsets: ['latin', 'cyrillic'], weight: ['400', '500', '700'] })
+export const roboto_mono = Roboto_Mono({ subsets: ['latin', 'cyrillic'], display: 'swap' })
