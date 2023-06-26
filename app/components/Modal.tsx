bc40c5f220e55eb43f63dc287308539066128182
Ivan Shulgin <multizookie@gmail.com>
2023-06-26 16:15:38 +0700

    Исправлен баг с отображением кнопки "закрыть"

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux'
 import { cartActions } from '../redux/cart'
 import { Dispatch, SetStateAction } from 'react'
 import styles from './TicketButtons.module.css'
+import ticketCardStyles from './ticketCard.module.css'
 
 export default function Modal({ id, setShowModal }:
   {id: string, setShowModal: Dispatch<SetStateAction<boolean>>}) {
@@ -11,8 +12,8 @@ export default function Modal({ id, setShowModal }:
 
   const modalContainer: HTMLElement = document.getElementById('modal') as HTMLElement
   const modalContent = (
-    <div className="modal-container">
-      <div className={`card g1`}>
+    <div className="modal-container" onClick={() => setShowModal(false)}>
+      <div className={`card g1`} onClick={e => e.stopPropagation()}>
         <p className={styles.modalTitle}>Удаление билета</p>
         <p className={styles.modalText}>Вы уверены, что хотите удалить билет?</p>
 
@@ -25,8 +26,8 @@ export default function Modal({ id, setShowModal }:
           </button>
         </div>
 
-        <button onClick={() => setShowModal(false)} className={`${styles.modalCloseButton} ${styles.deleteButton}`} type="button">
-          <div className={styles.deleteButtonIcon} />
+        <button onClick={() => setShowModal(false)} className={`${styles.modalCloseButton} ${ticketCardStyles.deleteButton}`} type="button">
+          <div className={ticketCardStyles.deleteButtonIcon} />
         </button>
       </div>
     </div>
